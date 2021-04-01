a948d2167f39147e8734246959faaa8af94d43c5
Shane Reed <shanem.reed@gmail.com>
2021-04-01 16:14:46 -0400

    Add files via upload

---
+++ website/js/grayscale.js
@@ -0,0 +1,41 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+function collapseNavbar() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+}
+
+//XKCD get current comic
+$(document).ready(function(){
+	$.getJSON("https://xkcd.com/info.0.json",function(json){
+		var $comic_current = jason[0].img;
+		$("#comic").attr("src", $comic_current);
+	});
+});
+
+$(window).scroll(collapseNavbar);
+$(document).ready(collapseNavbar);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
